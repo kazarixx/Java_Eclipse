e3b284f91c5e0976da6721a69e47e484a003f1a8
kazarixx <tua.ih8@gmail.com>
2022-01-25 23:37:10 +0900

    Java 4章

--- Ready02.java
+++ Ready02.java
@@ -1,5 +1,13 @@
+//クラスブロックの始まり
 public class Ready02 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	// メソッドブロックの始まり
+	public static void main(String[] args) {
+		// メソッドの処理
+		System.out.println("Hello World!");
+		System.out.println("Hello WEBCAMP!");
+	    System.out.println("Hello Java!");
+		// ↑文字列の出力
+	}
+	// メソッドブロックの終わり
 }
+//クラスブロックの終わり
