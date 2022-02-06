25859494830d1c24bfb5252e383c883cfe2d97a3
kazarixx <tua.ih8@gmail.com>
2022-02-07 00:04:28 +0900

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 777;
+        String str = "ウェブ　キャンプ";
+        System.out.println(str);
+        str = "一石二鳥";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
